4877d607f4dace9d79ec6006c3ad5dbcd2792d11
ArchuEthirajulu <112779437+ArchuEthirajulu@users.noreply.github.com>
2022-09-06 17:38:34 +0530

    Add files via upload

---
+++ week2.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class week2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup(); 
+		 ChromeDriver driver=new ChromeDriver(); 
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
